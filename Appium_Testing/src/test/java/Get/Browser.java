24c106fe0f0413b14797323b9675b785e2b87b2a
automationwitharavinda <aravindanath86@gmail.com>
2016-11-12 19:41:00 +0530

    List of web links

--- Appium_Testing/src/test/java/Get/Browser.java
+++ Appium_Testing/src/test/java/Get/Browser.java
@@ -1,5 +1,10 @@
 package Get;
+import java.util.Iterator;
+import java.util.List;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
  
 
@@ -15,7 +20,15 @@ public class Browser {
 		driver.manage().deleteAllCookies();
 		driver.get("https://www.google.co.in/");
 		String title = driver.getTitle();
-		
+		List<WebElement> links = driver.findElements(By.tagName("a"));
+		System.out.println("Links----->" + links);
+		for (Iterator<WebElement> iterator = links.iterator(); iterator.hasNext();) {
+			WebElement webElement = (WebElement) iterator.next();
+		//	System.out.println(webElement.getSize());
+			System.out.println(webElement.getText().trim().replace("", "---"));
+		//	System.out.println(iterator.next().getText());;
+			
+		}
 		System.out.println(title);
 		driver.close();
 		
